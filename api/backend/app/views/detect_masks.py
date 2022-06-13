c358621eb9d5675d93ac25e95f4f8796bc0dc622
starminalush <alica154323@gmail.com>
2022-06-13 14:47:53 +0500

    codestyle

--- api/backend/app/views/detect_masks.py
+++ api/backend/app/views/detect_masks.py
@@ -16,22 +16,28 @@ from torchvision import transforms
 
 load_dotenv()
 
-os.environ['MLFLOW_TRACKING_URI'] = os.getenv("MLFLOW_TRACKING_URI")
-os.environ['MLFLOW_S3_ENDPOINT_URL'] = os.getenv("MLFLOW_S3_ENDPOINT_URL")
-os.environ['AWS_ACCESS_KEY_ID'] = os.getenv("AWS_ACCESS_KEY_ID")
-os.environ['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
-mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
+os.environ["MLFLOW_TRACKING_URI"] = os.getenv("MLFLOW_TRACKING_URI")
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = os.getenv("MLFLOW_S3_ENDPOINT_URL")
+os.environ["AWS_ACCESS_KEY_ID"] = os.getenv("AWS_ACCESS_KEY_ID")
+os.environ["AWS_SECRET_ACCESS_KEY"] = os.getenv("AWS_SECRET_ACCESS_KEY")
+mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
 routes = aiohttp.web.RouteTableDef()
-detector: MtcnnDetector = MtcnnDetector(model_folder='/app/app/modules/mtcnn_model', ctx=mx.cpu(), num_worker=1,
-                                        accurate_landmark=True,
-                                        threshold=[0.7, 0.8, 0.9])
+detector: MtcnnDetector = MtcnnDetector(
+    model_folder="/app/app/modules/mtcnn_model",
+    ctx=mx.cpu(),
+    num_worker=1,
+    accurate_landmark=True,
+    threshold=[0.7, 0.8, 0.9],
+)
 
-model = mlflow.pytorch.load_model(os.getenv('MODEL_PATH'), map_location=torch.device('cpu'))
+model = mlflow.pytorch.load_model(
+    os.getenv("MODEL_PATH"), map_location=torch.device("cpu")
+)
 model.eval()
 
 
 def bytes_encode_image(image: np.ndarray) -> bytes:
-    return BytesIO(cv.imencode('.jpg', image)[1].tostring()).read()
+    return BytesIO(cv.imencode(".jpg", image)[1].tostring()).read()
 
 
 def preprocess_image(img: bytes) -> np.ndarray:
@@ -44,7 +50,7 @@ transform = transforms.Compose(
     [
         transforms.Resize([224, 224]),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]
 )
 
@@ -56,7 +62,7 @@ def prepare_image(image: np.ndarray) -> torch.Tensor:
     return tensor
 
 
-@routes.post('/detect_image')
+@routes.post("/detect_image")
 async def detect_image(request: aiohttp.web.Request):
     """
     метод, который получает картинку, находит на ней лица,
@@ -72,11 +78,11 @@ async def detect_image(request: aiohttp.web.Request):
 
     image = await reader.next()
     if not image:
-        return aiohttp.web.HTTPBadRequest(reason='Please provide image file.')
+        return aiohttp.web.HTTPBadRequest(reason="Please provide image file.")
     if image:
-        img_content: bytes = await  image.read()
+        img_content: bytes = await image.read()
     else:
-        return {'error:wrong image'}, 400
+        return {"error:wrong image"}, 400
 
     im: np.ndarray = preprocess_image(img_content)
 
@@ -85,7 +91,7 @@ async def detect_image(request: aiohttp.web.Request):
         bbox: List = i
         bbox = [int(i) for i in bbox]
         bbox = [0 if i < 0 else i for i in bbox]
-        croped_frame: np.ndarray = im[bbox[1]: bbox[3], bbox[0]: bbox[2]]
+        croped_frame: np.ndarray = im[bbox[1] : bbox[3], bbox[0] : bbox[2]]
 
         image_tensor: torch.Tensor = prepare_image(croped_frame)
         result: torch.Tensor = model(image_tensor)
@@ -93,14 +99,16 @@ async def detect_image(request: aiohttp.web.Request):
         _, predicted = torch.max(result.data, 1)
         result: int = predicted.item()
         color: Tuple = ()
-        text: str = ''
+        text: str = ""
         if result == 1:
             color = (0, 0, 255)
-            text = 'without_mask'
+            text = "without_mask"
         if result == 0:
             color = (0, 255, 0)
-            text = 'with_mask'
+            text = "with_mask"
         cv.rectangle(im, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 5)
-        cv.putText(im, text, (bbox[0], bbox[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+        cv.putText(
+            im, text, (bbox[0], bbox[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, color, 2
+        )
     result_image: bytes = bytes_encode_image(im)
     return aiohttp.web.Response(body=result_image, content_type="image/jpeg")
